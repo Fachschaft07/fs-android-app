ee3f74ea032bb8c70c5f6bdc7869ad24b2bb5477
Fabio Hellmann <fhellmann93@googlemail.com>
2016-05-27 21:52:02 +0200

    Replace custom timetable with WeekView library

--- app/src/main/java/edu/hm/cs/fs/app/ui/timetable/TimetableFragment.java
+++ app/src/main/java/edu/hm/cs/fs/app/ui/timetable/TimetableFragment.java
@@ -3,21 +3,31 @@ package edu.hm.cs.fs.app.ui.timetable;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.TouchDelegate;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
 import com.fk07.R;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -33,11 +43,7 @@ import edu.hm.cs.fs.common.model.Lesson;
  * @author Fabio
  */
 public class TimetableFragment extends BaseFragment<TimetablePresenter> implements ITimetableView,
-        TimetableAdapter.OnItemClickListener, Toolbar.OnMenuItemClickListener {
-
-    private static final int PORTRAIT_DAY_COUNT = 2;
-
-    private static final int LANDSCAPE_DAY_COUNT = 5;
+        Toolbar.OnMenuItemClickListener, MonthLoader.MonthChangeListener, WeekView.EventClickListener {
 
     @Bind(R.id.toolbar)
     Toolbar mToolbar;
@@ -45,10 +51,13 @@ public class TimetableFragment extends BaseFragment<TimetablePresenter> implemen
     @Bind(R.id.swipeContainer)
     SwipeRefreshLayout mSwipeRefreshLayout;
 
-    @Bind(R.id.listView)
-    RecyclerView mListView;
+    @Bind(R.id.weekView)
+    WeekView mWeekView;
 
-    private TimetableAdapter mAdapter;
+    private List<Lesson> mLessons;
+    private Map<Integer, Lesson> mContent = new HashMap<>();
+
+    private int mVisibleDays = 2;
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -65,17 +74,20 @@ public class TimetableFragment extends BaseFragment<TimetablePresenter> implemen
         mToolbar.inflateMenu(R.menu.timetable);
         mToolbar.setOnMenuItemClickListener(this);
 
-        final int numberOfDays;
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            numberOfDays = PORTRAIT_DAY_COUNT;
-        } else {
-            numberOfDays = LANDSCAPE_DAY_COUNT;
-        }
+        mSwipeRefreshLayout.setEnabled(false);
 
-        mAdapter = new TimetableAdapter(getActivity(), numberOfDays);
-        mAdapter.setOnItemClickListener(this);
-        mListView.setAdapter(mAdapter);
-        mListView.setLayoutManager(new GridLayoutManager(getActivity(), numberOfDays + 1));
+        mWeekView.setMonthChangeListener(this);
+        mWeekView.setOnEventClickListener(this);
+        mWeekView.setShowNowLine(true);
+        mWeekView.setOverlappingEventGap(2);
+        mWeekView.setNumberOfVisibleDays(mVisibleDays);
+        mWeekView.goToHour(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
+        mWeekView.setScrollListener(new WeekView.ScrollListener() {
+            @Override
+            public void onFirstVisibleDayChanged(Calendar newFirstVisibleDay, Calendar oldFirstVisibleDay) {
+
+            }
+        });
 
         setPresenter(new TimetablePresenter(getActivity(), this));
         getPresenter().loadTimetable(false);
@@ -94,20 +106,19 @@ public class TimetableFragment extends BaseFragment<TimetablePresenter> implemen
     @Override
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menu_prev_day:
-                mAdapter.prevDay();
-                return true;
             case R.id.menu_today:
-                mAdapter.today();
-                return true;
-            case R.id.menu_next_day:
-                mAdapter.nextDay();
+                mWeekView.goToToday();
+                mWeekView.goToHour(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
                 return true;
             case R.id.menu_week_view:
-                getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                mVisibleDays = 5;
+                mWeekView.setNumberOfVisibleDays(mVisibleDays);
+                item.setChecked(true);
                 return true;
             case R.id.menu_two_day_view:
-                getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                mVisibleDays = 2;
+                mWeekView.setNumberOfVisibleDays(mVisibleDays);
+                item.setChecked(true);
                 return true;
             case R.id.menu_lesson_selection:
                 MainActivity.getNavigator().goTo(new TimetableEditorFragment());
@@ -119,7 +130,9 @@ public class TimetableFragment extends BaseFragment<TimetablePresenter> implemen
 
     @Override
     public void showContent(List<Lesson> content) {
-        mAdapter.setData(content);
+        mLessons = content;
+        mContent.clear();
+        mWeekView.notifyDatasetChanged();
     }
 
     @Override
@@ -128,27 +141,54 @@ public class TimetableFragment extends BaseFragment<TimetablePresenter> implemen
     }
 
     @Override
-    public void onItemClicked(@NonNull Lesson lesson) {
+    public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+        final List<WeekViewEvent> events = new ArrayList<>();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(newYear, newMonth - 1, 1);
+
+        final int maxDaysOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        for (int dayOfMonth = 1; dayOfMonth <= maxDaysOfMonth; dayOfMonth++) {
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+            for (Lesson lesson : mLessons) {
+                if(lesson.getDay().getCalendarId() == calendar.get(Calendar.DAY_OF_WEEK)) {
+                    Calendar start = Calendar.getInstance();
+                    start.set(newYear, newMonth - 1, dayOfMonth, lesson.getHour(), lesson.getMinute(), 0);
+
+                    Calendar end = Calendar.getInstance();
+                    end.setTimeInMillis(start.getTimeInMillis());
+                    end.add(Calendar.MINUTE, 90);
+
+                    final WeekViewEvent weekViewEvent = new WeekViewEvent(mContent.size(), lesson.getModule().getName(), lesson.getRoom(), start, end);
+                    events.add(weekViewEvent);
+                    mContent.put(mContent.size(), lesson);
+                }
+            }
+        }
+
+        return events;
+    }
+
+    @Override
+    public void onEventClick(WeekViewEvent event, RectF eventRect) {
+        final Lesson foundLesson = mContent.get((int) event.getId());
+
+        if(foundLesson == null || TextUtils.isEmpty(foundLesson.getModule().getId()) || foundLesson.getModule().getId().contains(" ")) {
+            Snackbar.make(mWeekView, R.string.no_detail_subject_info, Snackbar.LENGTH_SHORT);
+            return;
+        }
+
         Bundle arguments = new Bundle();
-        arguments.putString(TimetableLessonFragment.ARG_MODULE_ID, lesson.getModule().getId());
-        if(lesson.getTeacher() != null) {
-            arguments.putString(TimetableLessonFragment.ARG_TEACHER_ID, lesson.getTeacher().getId());
+        arguments.putString(TimetableLessonFragment.ARG_MODULE_ID, foundLesson.getModule().getId());
+        if(foundLesson.getTeacher() != null) {
+            arguments.putString(TimetableLessonFragment.ARG_TEACHER_ID, foundLesson.getTeacher().getId());
         }
 
         final TimetableLessonFragment fragment = new TimetableLessonFragment();
         fragment.setArguments(arguments);
 
         MainActivity.getNavigator().goTo(fragment);
-    }
-
-    @Override
-    public void onEmptyClicked(@NonNull Day day, @NonNull Time time) {
-        // TODO Add lessons manually
-    }
-
-    @Override
-    public void onLessonSelection(@NonNull Lesson lesson) {
-        mAdapter.notifyDataSetChanged();
     }
 
     @Override
